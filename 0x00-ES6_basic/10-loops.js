ecee41df020c19d33943dbe1b5f9593b0d9ee354
unknown <henyvibe@gmail.com>
2022-12-19 19:30:40 +0300

    ES6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
+    // eslint-disable-next-line
   for (const idx in array) {
     array[array.indexOf(idx)] = appendString + idx;
   }
